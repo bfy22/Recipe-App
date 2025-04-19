49a2e73b3dc37e3a625752b51e9f1d56c5a5527a
bfyehia77 <bfyehia77@gmail.com>
2025-04-19 21:53:25 +0400

    start of JS code, calling API and fetching JSON recipe data

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ const searchForm = document.querySelector('.js-form');
 const searchResultObj = document.querySelector('.js-search-results');
 const projectContainer  = document.querySelector('.js-container');
 const API_Key = 'd356faf76ff245fc87c936fbaa616aeb';
-const baseURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_Key}`
 
 let userSearchQuery = '';
+
 
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -12,6 +12,15 @@ searchForm.addEventListener('submit', (event) => {
   callAPI();
 });
 
+
 async function callAPI() {
-  
+  const baseURL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_Key}&query=${userSearchQuery}&addRecipeInformation=true&number=15`
+  const response = await fetch(baseURL);
+  const fetchedData = await response.json(); /*json to obj method for recipe fetches*/
+  console.log(fetchedData);
 }
+
+function renderSearchResults() {
+
+}
+
