a50e8af666a93b2fadd45c9b9b6e3224da564bef
bfyehia77 <bfyehia77@gmail.com>
2025-04-27 23:31:08 +0400

    untested capatilize function

--- scripts/utils/capitlizeEveryWord.js
+++ scripts/utils/capitlizeEveryWord.js
@@ -1,4 +1,17 @@
 export function capitalizeEveryWord(string) {
+  const excludeCases = ["and", "with"];
   const targetedWords = string.split(' ');
-  console.log(targetedWords);
+  let modifiedString = [];
+  let modifiedWord = '';
+
+   targetedWords.forEach((word) => {
+    if(excludeCases.includes(word.toLowerCase())) {
+      modifiedString.push(word.toLowerCase());
+    } else {
+      modifiedWord = word.charAt(0).toUpperCase() + word.slice(1);
+      modifiedString.push(modifiedWord); 
+    }
+  });
+  const completeString = modifiedString.join(' ');
+  return completeString;
 }
