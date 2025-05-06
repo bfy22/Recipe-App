7b47c12b77bd549f8edf6711a7782b4968df5a35
bfyehia77 <bfyehia77@gmail.com>
2025-05-06 12:03:29 +0400

    heart icon animation function

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -11,7 +11,8 @@ export function manageFavorites(recipeDataArray) {
   document.body.addEventListener('click', event => {
     const favoriteButton = event.target.closest('.js-favorite-button')
     if(!favoriteButton) return;
-    
+
+    const heartIcon = favoriteButton.querySelector('ion-icon');
     const recipeID = event.target.getAttribute('data-item-id'); 
     const recipe = recipeDataArray.find(recipe => recipe.id == recipeID); 
     if (!recipe) return; 
@@ -21,9 +22,14 @@ export function manageFavorites(recipeDataArray) {
 
     if (favIndex !== -1) {
       favoriteRecipes.splice(favIndex, 1);
+      heartIcon.setAttribute('name', 'heart-outline');
     } else {
       favoriteRecipes.push(recipe);
+      heartIcon.setAttribute('name', 'heart');
     }
+
+    heartIcon.classList.add('bounce');
+    setTimeout(() => heartIcon.classList.remove('bounce'), 400);
     console.log(favoriteRecipes); 
   });
 
