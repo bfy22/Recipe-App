84c2f8371bee0b3726731ad24080db3e6a7be768
bfyehia77 <bfyehia77@gmail.com>
2025-05-19 13:49:25 +0400

    comments

--- scripts/tests/capitalizeTest.js
+++ scripts/tests/capitalizeTest.js
@@ -1,5 +1,8 @@
 import { capitalizeEveryWord } from "../utils/capitalizeEveryWord.js";
 
+//unit tests for capitalizing recipe title function
+
+//1-2 test cases
 if (capitalizeEveryWord('bisso') === 'Bisso') {
   console.log('passed');
 } else {
@@ -12,6 +15,7 @@ if (capitalizeEveryWord('grilled cheese and tomato soup') === 'Grilled Cheese an
   console.log('failed');
 }
 
+//edge cases
 if (capitalizeEveryWord('Grilled-cheese and tomato soup') === 'Grilled Cheese and Tomato Soup') {
   console.log('passed');
 } else {
